fb6a9cc74797c75185169af659c79d29f392b97a
Ezequiel Rasgido <ezequielrasgido@icloud.com>
2023-04-10 18:54:28 -0300

    Initial Scaffolding

---
+++ XBike/XBike/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  XBike
+//
+//  Created by Ezequiel Rasgido on 10/04/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
