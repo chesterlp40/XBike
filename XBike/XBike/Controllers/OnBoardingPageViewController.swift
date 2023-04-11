3dcfd665c821c36ba7560029c6d86b215b7a73ec
Ezequiel Rasgido <42559878+chesterlp40@users.noreply.github.com>
2023-04-11 01:33:28 -0300

    Add OnBoarding Pages (#3)
    * Add OnBoarding Pages
    
    * More changes
    
    * Add let´s ride button

---
+++ XBike/XBike/Controllers/OnBoardingPageViewController.swift
@@ -0,0 +1,147 @@
+//
+//  OnBoardingPageViewController.swift
+//  XBike
+//
+//  Created by Ezequiel Rasgido on 10/04/2023.
+//
+
+import UIKit
+
+class OnBoardingPageViewController: UIPageViewController {
+    lazy var pages: [UIViewController] = {
+        return [
+            PageViewController(
+                imageName: "easy",
+                subTitle: "Extremely simple to use"
+            ),
+            PageViewController(
+                imageName: "distance",
+                subTitle: "Track your time and distance"
+            ),
+            PageViewController(
+                imageName: "boastful",
+                subTitle: "See your progress and challenge yourself!",
+                isLastPage: true
+            )
+        ]
+    }()
+    private let pageControl = UIPageControl()
+    private let initialPage = 0
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.setupComponents()
+    }
+    
+    private func setupComponents() {
+        dataSource = self
+        delegate = self
+        
+        self.navigationController?.setNavigationBarHidden(
+            true,
+            animated: false
+        )
+        self.pageControl.addTarget(
+            self,
+            action: #selector(pageControlTapped(_:)),
+            for: .valueChanged
+        )
+        self.setViewControllers(
+            [self.pages[self.initialPage]],
+            direction: .forward,
+            animated: true,
+            completion: nil
+        )
+        self.setupPageControl()
+        
+    }
+    
+    private func setupPageControl() {
+        self.pageControl.translatesAutoresizingMaskIntoConstraints = false
+        self.pageControl.currentPageIndicatorTintColor = .black
+        self.pageControl.pageIndicatorTintColor = .systemGray2
+        self.pageControl.numberOfPages = self.pages.count
+        self.pageControl.currentPage = self.initialPage
+        self.view.addSubview(self.pageControl)
+        
+        NSLayoutConstraint.activate([
+            self.pageControl.widthAnchor.constraint(equalTo: view.widthAnchor),
+            self.pageControl.heightAnchor.constraint(equalToConstant: 20),
+            self.view.bottomAnchor.constraint(
+                equalToSystemSpacingBelow: self.pageControl.bottomAnchor,
+                multiplier: 3
+            ),
+        ])
+    }
+    
+    @objc
+    func pageControlTapped(
+        _ sender: UIPageControl
+    ) {
+        self.setViewControllers(
+            [self.pages[sender.currentPage]],
+            direction: .forward,
+            animated: true,
+            completion: nil
+        )
+    }
+}
+
+// MARK: - DataSources
+
+extension OnBoardingPageViewController: UIPageViewControllerDataSource {
+    
+    func pageViewController(
+        _ pageViewController: UIPageViewController,
+        viewControllerBefore viewController: UIViewController
+    ) -> UIViewController? {
+        guard
+            let currentIndex = self.pages.firstIndex(of: viewController)
+        else {
+            return nil
+        }
+        
+        if currentIndex == 0 {
+            return self.pages.last
+        } else {
+            return self.pages[currentIndex - 1]
+        }
+    }
+    
+    func pageViewController(
+        _ pageViewController: UIPageViewController,
+        viewControllerAfter viewController: UIViewController
+    ) -> UIViewController? {
+        guard
+            let currentIndex = self.pages.firstIndex(of: viewController)
+        else {
+            return nil
+        }
+
+        if currentIndex < self.pages.count - 1 {
+            return self.pages[currentIndex + 1]
+        } else {
+            return self.pages.first
+        }
+    }
+}
+
+// MARK: - Delegates
+
+extension OnBoardingPageViewController: UIPageViewControllerDelegate {
+    
+    func pageViewController(
+        _ pageViewController: UIPageViewController,
+        didFinishAnimating finished: Bool,
+        previousViewControllers: [UIViewController],
+        transitionCompleted completed: Bool
+    ) {
+        guard
+            let viewControllers = pageViewController.viewControllers,
+            let currentIndex = pages.firstIndex(of: viewControllers[0])
+        else {
+            return
+        }
+        self.pageControl.currentPage = currentIndex
+    }
+}
---
+++ XBike/XBike/Controllers/PageViewController.swift
@@ -0,0 +1,64 @@
+//
+//  PageViewController.swift
+//  XBike
+//
+//  Created by Ezequiel Rasgido on 10/04/2023.
+//
+
+import UIKit
+
+class PageViewController: UIViewController {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var subTitleLabel: UILabel!
+    @IBOutlet weak var actionButton: UIButton!
+    
+    private let imageName: String
+    private let subTitle: String
+    private let isLastPage: Bool
+    
+    init(
+        imageName: String,
+        subTitle: String,
+        isLastPage: Bool? = nil
+    ) {
+        self.imageName = imageName
+        self.subTitle = subTitle
+        self.isLastPage = isLastPage ?? false
+        super.init(
+            nibName: nil,
+            bundle: nil
+        )
+    }
+    
+    required init?(
+        coder: NSCoder
+    ) {
+        fatalError(
+            "init(coder:) has not been implemented"
+        )
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.setupComponents()
+    }
+    
+    private func setupComponents() {
+        self.imageView.image = UIImage(
+            named: self.imageName
+        )
+        self.subTitleLabel.text = self.subTitle
+        self.actionButton.layer.cornerRadius = 8
+        self.actionButton.titleLabel?.font = UIFont(
+            name: "Kohinoor Telugu Light",
+            size: 20
+        )
+        self.actionButton.isHidden = !self.isLastPage
+    }
+    
+    @IBAction func actionButtonPressed(
+        _ sender: UIButton
+    ) {
+        
+    }
+}
