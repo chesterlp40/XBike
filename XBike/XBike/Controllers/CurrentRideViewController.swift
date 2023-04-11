7e96974e165f6d978f84b888b1b3c956cd1e044f
Ezequiel Rasgido <42559878+chesterlp40@users.noreply.github.com>
2023-04-11 03:15:53 -0300

    Dismiss on boarding logic (#4)

---
+++ XBike/XBike/Controllers/CurrentRideViewController.swift
@@ -0,0 +1,16 @@
+//
+//  CurrentRideViewController.swift
+//  XBike
+//
+//  Created by Ezequiel Rasgido on 11/04/2023.
+//
+
+import UIKit
+
+class CurrentRideViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Current Ride"
+    }
+}
