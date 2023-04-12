f1a282cc72a9d2ce60af39d7609fb8fa9b49dbba
Ezequiel Rasgido <ezequielrasgido@icloud.com>
2023-04-12 02:08:44 -0300

    Add My Progress Layout

--- XBike/XBike/Controllers/MyProgressViewController.swift
+++ XBike/XBike/Controllers/MyProgressViewController.swift
@@ -8,9 +8,30 @@
 import UIKit
 
 class MyProgressViewController: BaseViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "My Progress"
+    }
+}
+
+extension MyProgressViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(
+        _ tableView: UITableView,
+        numberOfRowsInSection section: Int
+    ) -> Int {
+        return 3
+    }
+    
+    func tableView(
+        _ tableView: UITableView,
+        cellForRowAt indexPath: IndexPath
+    ) -> UITableViewCell {
+        // let cell = self.tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        // cell.textLabel?.text = "Hello World"
+        return UITableViewCell() // cell
     }
 }
---
+++ XBike/XBike/Controllers/RideTimesCell.swift
@@ -0,0 +1,23 @@
+//
+//  RideTimesCell.swift
+//  XBike
+//
+//  Created by Ezequiel Rasgido on 12/04/2023.
+//
+
+import UIKit
+
+class RideTimesCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
