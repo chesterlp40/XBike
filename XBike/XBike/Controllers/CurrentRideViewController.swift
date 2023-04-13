d6b918560b607a44defb4a1a31e3943fbc1a52d4
Ezequiel Rasgido <ezequielrasgido@icloud.com>
2023-04-12 22:41:10 -0300

    Add Current Ride Modal

--- XBike/XBike/Controllers/CurrentRideViewController.swift
+++ XBike/XBike/Controllers/CurrentRideViewController.swift
@@ -12,14 +12,56 @@ import UIKit
 class CurrentRideViewController: BaseViewController, CLLocationManagerDelegate {
     
     @IBOutlet weak var mapView: GMSMapView!
-
-    private var locationManager = CLLocationManager()
-
+    
+    private(set) var modal: RideTrackingModalView?
+    private(set) var locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Current Ride"
         
         self.locationManager.delegate = self
+        self.setupComponents()
+    }
+    
+    private func setupComponents() {
+        self.modal = RideTrackingModalView(
+            frame: self.view.frame
+        )
+        let image = UIImage(systemName: "plus")
+        let rightBarbutton = UIBarButtonItem(
+            image: image,
+            style: .plain,
+            target: self,
+            action: #selector(onTrackPressed)
+        )
+        rightBarbutton.tintColor = .white
+        self.navigationItem.rightBarButtonItem = rightBarbutton
+    }
+    
+    @objc
+    func onTrackPressed() {
+        guard let modal = self.modal else {
+            return
+        }
+        modal.startButton.addTarget(self, action: #selector(startButtonPressed), for: .touchUpInside)
+        modal.stopButton.addTarget(self, action: #selector(stopButtonPressed), for: .touchUpInside)
+        self.view.addSubview(modal)
+    }
+    
+    @objc
+    func startButtonPressed() {
+        guard let modal = self.modal else {
+            return
+        }
+    }
+    
+    @objc
+    func stopButtonPressed() {
+        guard let modal = self.modal else {
+            return
+        }
+        modal.removeFromSuperview()
     }
     
     func locationManagerDidChangeAuthorization(
